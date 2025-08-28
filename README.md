749d80ac37b20a423a4d9df53247a8b3ccc7709e
noexcept <zhaowenwei@outlook.com>
2025-08-28 11:28:30 +0800

    init macos environment

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # LearnFFmpeg
+
+是一个macOS环境下学习 FFmpeg 的项目
+## 安装依赖
+### 安装FFmpeg
+brew install ffmpg
+### 安装Qt
+brew install qt
---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by noexcept on 2025/8/28.
+//
+
+#include <QApplication>
+#include <QWidget>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QWidget widget;
+    widget.show();
+    return app.exec();
+}
